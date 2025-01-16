8c2de7af887f52f50673519334f7fc4dc759ddca
MagicXL2022 <104300642+MagicXL2022@users.noreply.github.com>
2025-01-16 15:13:53 +0800

    去除授权信息

--- Android/app/src/main/java/org/easydarwin/easypusher/AboutActivity.java
+++ Android/app/src/main/java/org/easydarwin/easypusher/AboutActivity.java
@@ -31,60 +31,60 @@ public class AboutActivity extends AppCompatActivity implements Toolbar.OnMenuIt
         binding.mainToolbar.setNavigationIcon(R.drawable.com_back);
 
         binding.version.setText("EasyRTMP Android 推流器");
-        binding.version.append("(");
-
-        SpannableString spannableString;
-        if (EasyApplication.activeDays >= 9999) {
-            spannableString = new SpannableString("激活码永久有效");
-            spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorGREEN)),
-                    0,
-                    spannableString.length(),
-                    Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-        } else if (EasyApplication.activeDays > 0) {
-            spannableString = new SpannableString(String.format("激活码还剩%d天可用", EasyApplication.activeDays));
-            spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorYELLOW)),
-                    0,
-                    spannableString.length(),
-                    Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-        } else {
-            spannableString = new SpannableString(String.format("激活码已过期(%d)", EasyApplication.activeDays));
-            spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorRED)),
-                    0,
-                    spannableString.length(),
-                    Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-        }
-
-        binding.version.append(spannableString);
-        binding.version.append(")");
-
-        binding.serverTitle.setText("EasyDSS RTMP流媒体服务器：\n");
-        binding.serverTitle.setMovementMethod(LinkMovementMethod.getInstance());
-
-        spannableString = new SpannableString("http://www.easydss.com");
-        //设置下划线文字
-        spannableString.setSpan(new URLSpan("http://www.easydss.com"), 0, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-
-        //设置文字的前景色
-        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorTheme)),
-                0,
-                spannableString.length(),
-                Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-
-        binding.serverTitle.append(spannableString);
-
-        binding.playerTitle.setText("EasyPlayerPro全功能播放器：\n");
-        binding.playerTitle.setMovementMethod(LinkMovementMethod.getInstance());
-        spannableString = new SpannableString("https://github.com/EasyDSS/EasyPlayerPro");
-        //设置下划线文字
-        spannableString.setSpan(new URLSpan("https://github.com/EasyDSS/EasyPlayerPro"), 0, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-
-        //设置文字的前景色
-        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorTheme)),
-                0,
-                spannableString.length(),
-                Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-
-        binding.playerTitle.append(spannableString);
+//        binding.version.append("(");
+//
+//        SpannableString spannableString;
+//        if (EasyApplication.activeDays >= 9999) {
+//            spannableString = new SpannableString("激活码永久有效");
+//            spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorGREEN)),
+//                    0,
+//                    spannableString.length(),
+//                    Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+//        } else if (EasyApplication.activeDays > 0) {
+//            spannableString = new SpannableString(String.format("激活码还剩%d天可用", EasyApplication.activeDays));
+//            spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorYELLOW)),
+//                    0,
+//                    spannableString.length(),
+//                    Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+//        } else {
+//            spannableString = new SpannableString(String.format("激活码已过期(%d)", EasyApplication.activeDays));
+//            spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorRED)),
+//                    0,
+//                    spannableString.length(),
+//                    Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+//        }
+//
+//        binding.version.append(spannableString);
+//        binding.version.append(")");
+//
+//        binding.serverTitle.setText("EasyDSS RTMP流媒体服务器：\n");
+//        binding.serverTitle.setMovementMethod(LinkMovementMethod.getInstance());
+//
+//        spannableString = new SpannableString("http://www.easydss.com");
+//        //设置下划线文字
+//        spannableString.setSpan(new URLSpan("http://www.easydss.com"), 0, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+//
+//        //设置文字的前景色
+//        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorTheme)),
+//                0,
+//                spannableString.length(),
+//                Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+//
+//        binding.serverTitle.append(spannableString);
+//
+//        binding.playerTitle.setText("EasyPlayerPro全功能播放器：\n");
+//        binding.playerTitle.setMovementMethod(LinkMovementMethod.getInstance());
+//        spannableString = new SpannableString("https://github.com/EasyDSS/EasyPlayerPro");
+//        //设置下划线文字
+//        spannableString.setSpan(new URLSpan("https://github.com/EasyDSS/EasyPlayerPro"), 0, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+//
+//        //设置文字的前景色
+//        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorTheme)),
+//                0,
+//                spannableString.length(),
+//                Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+//
+//        binding.playerTitle.append(spannableString);
     }
 
     @Override
